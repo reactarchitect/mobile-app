dd31ad684d8eb25cb2ef6e920ba734da218c0a2f
Kashyap Patel <kashyap.patel@gtcsys.com>
2021-09-04 16:34:41 +0530

    Redux Integration & Verification With ToDo CRUD Operation

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ import React, { useEffect } from 'react';
+ import {
+   StyleSheet
+ } from 'react-native';
+ 
+ import {
+   Colors,
+ } from 'react-native/Libraries/NewAppScreen';
+ import Landing from './app/Components/Landing';
+ import { Provider } from 'react-redux';
+ import { getPersistor } from '@rematch/persist';
+ import { PersistGate } from 'redux-persist/lib/integration/react';
+ import store from './app/Redux/Store';
+ 
+ const App: () => React$Node = () => {
+ 
+   useEffect(() => {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+   }, []);
+ 
+   return (
+     <>
+       <Provider store={store}>
+         <PersistGate persistor={getPersistor()}>
+           <Landing />
+         </PersistGate>
+       </Provider>
+     </>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   scrollView: {
+     backgroundColor: Colors.lighter,
+   },
+   engine: {
+     position: 'absolute',
+     right: 0,
+   },
+   body: {
+     backgroundColor: Colors.white,
+   },
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+     color: Colors.black,
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+     color: Colors.dark,
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+   footer: {
+     color: Colors.dark,
+     fontSize: 12,
+     fontWeight: '600',
+     padding: 4,
+     paddingRight: 12,
+     textAlign: 'right',
+   },
+ });
+ 
+ export default App;
+ 
---
+++ app/Components/Landing/index.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { SafeAreaView, Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
+
+function Landing(props) {
+
+    const [taskName, setTaskName] = useState("");
+    const [taskID, setTaskID] = useState(undefined);
+
+    useEffect(() => {
+        props.clearToDo();
+        // props.fetchToDo();
+        // props.fetchPosts();
+    }, []);
+
+    const onPressSubmit = () => {
+        if (taskID) {
+            props.updateToDo({
+                id: taskID,
+                name: taskName
+            });
+        } else {
+            console.log('props', props);
+            props.insertToDo({
+                taskName
+            });
+        }
+    }
+    const onPressTask = (task) => {
+        console.log('---control here', task);
+        setTaskName(task.name);
+        setTaskID(task.id);
+    }
+    const onPressDeleteTask = (task) => {
+        props.deleteToDo({
+            id: task.id
+        });
+    }
+
+    const TaskItem = (item) => {
+        return (
+            <View style={{ flex: 1, backgroundColor: 'lightblue' }}>
+                <Text>{item.name}</Text>
+                <Text>{item.id}</Text>
+            </View>
+        )
+    }
+
+    const renderItem = ({ item }) => (
+        <View style={{borderWidth:1, borderColor: 'lightgray'}}>
+            <Text>{item.name}</Text>
+            <Text>{item.id}</Text>
+            <TouchableOpacity style={{ width: 100, height: 35 }} onPress={() => onPressTask(item)}>
+                <Text>Edit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={{ width: 100, height: 35 }} onPress={() => onPressDeleteTask(item)}>
+                <Text>Delete</Text>
+            </TouchableOpacity>
+        </View>
+    );
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <Text>{props.loading && "Loading!!!"}</Text>
+            <TextInput
+                style={{ width: 300, height: 40 }}
+                onChangeText={(value) => setTaskName(value)}
+                value={taskName}
+                placeholder="Task Name"
+            />
+            <TouchableOpacity style={{ width: 100, height: 35 }} onPress={() => onPressSubmit()}>
+                <Text>Submit</Text>
+            </TouchableOpacity>
+            <View style={{ flex: 1 }}>
+
+                <FlatList
+                    data={props.arrToDo}
+                    renderItem={renderItem}
+                    showsVerticalScrollIndicator={false}
+                />
+            </View>
+            <View>
+                {/* <Text>{!props.loading && JSON.stringify(props.arrToDo)}</Text>
+            <Text>{props.loadingPosts && "Loading Posts !!!"}</Text>
+            <Text>{!props.loadingPosts && JSON.stringify(props.arrPosts)}</Text> */}
+            </View>
+        </SafeAreaView>
+    );
+}
+
+const mapStateToProps = ({ ToDo, loading }) => ({
+    arrToDo: ToDo.arrToDo,
+    loading: loading.effects.ToDo.fetchToDo,
+});
+
+const mapDispatchToProps = ({
+    ToDo: { fetchToDo, insertToDo, updateToDo, deleteToDo, clearToDo }
+}) => ({
+    fetchToDo: () => fetchToDo(),
+    insertToDo: (payload) => insertToDo(payload),
+    updateToDo: (payload) => updateToDo(payload),
+    deleteToDo: (payload) => deleteToDo(payload),
+    clearToDo: () => clearToDo(),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Landing);
